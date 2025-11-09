b00c7e32eef4952071374e588087c1c8fcda3418
sudami <zheng.zhiwen@mone-pla.co.jp>
2025-11-09 12:56:48 +0900

    fix errors

--- src/main/java/com/salary/calculator/config/SecurityConfig.java
+++ src/main/java/com/salary/calculator/config/SecurityConfig.java
@@ -46,7 +46,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "http://localhost:5173"));
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "http://localhost:5173", "http://salary.521125.best", "https://salary.521125.best"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
         configuration.setAllowCredentials(true);
